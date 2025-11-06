dd88c65c992381fb2c78480917e90d98a18d4946
Fraxoo <fraxo.officiel@gmail.com>
2025-11-06 14:07:40 +0100

    first commit

---
+++ back-end/database.mjs
@@ -0,0 +1,40 @@
+import { Sequelize, DataTypes } from "sequelize";
+
+
+export async function loadSequelize() {
+    try{
+        const login = {
+            databse: "app-database",
+            username: "root",
+            password: "root"
+        }
+
+        const sequelize = new Sequelize(login.databse,login.username,login.password,{
+            host: '127.0.0.1',
+            dialect: 'mysql'
+        })
+
+        const User = sequelize.define("User",{
+            username: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false
+            }
+        })
+
+
+        return sequelize;
+    } catch (error){
+        console.log(error);
+        
+    }
+}
