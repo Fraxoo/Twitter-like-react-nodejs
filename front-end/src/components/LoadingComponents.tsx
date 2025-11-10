ddfb8f3cc0c84a7fb5910c92c19a644010a46e73
Fraxoo <fraxo.officiel@gmail.com>
2025-11-10 11:20:40 +0100

    added components and pages to frond end

---
+++ front-end/src/components/LoadingComponents.tsx
@@ -0,0 +1,7 @@
+import { ThreeDot } from "react-loading-indicators";
+
+export default function Loading() {
+    return (
+        <ThreeDot color="#04b9aaff" size="medium" text="" textColor="" />
+    )
+}
---
+++ front-end/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Loading from "../components/LoadingComponents"
+
+
+
+export default function Home() {
+
+
+    return (
+        <div>
+            <Loading />
+        </div>
+    )
+}
