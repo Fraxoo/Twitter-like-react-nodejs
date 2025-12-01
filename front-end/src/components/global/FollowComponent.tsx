459494cf8f6ddd5dcd2ff4a4c1bd55065c482b9a
Fraxoo <fraxo.officiel@gmail.com>
2025-12-01 11:52:01 +0100

    added follow component

---
+++ front-end/src/components/global/FollowComponent.tsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react"
+
+
+
+export default function FollowComponent({ userId }: { userId: number }) {
+
+    const [isFollowed, setIsFollowed] = useState(false);
+    const [isHovering, setIsHovering] = useState(false);
+
+
+    useEffect(() => {
+
+        const getIsFollowed = async () => {
+            try {
+                const res = await fetch(`http://localhost:8000/follow/get/${userId}/isfollowed`, {
+                    method: "GET",
+                    headers: { "Content-Type": "application/json" },
+                    credentials: "include",
+                });
+
+                const data = await res.json();
+
+                if (!res.ok) {
+                    return;
+                }
+                setIsFollowed(data)
+            } catch (err) {
+                console.error(err)
+            }
+        }
+
+        getIsFollowed();
+    }, [userId])
+
+    const handleClick = async () => {
+        try {
+            if (isFollowed) {
+                const res = await fetch(`http://localhost:8000/follow/remove/${userId}`, {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    credentials: "include",
+                });
+
+                const data = await res.json();
+                console.log(data);
+
+                if (!res.ok) {
+                    console.error(res);
+                    return;
+                }
+                setIsFollowed(false);
+            } else {
+                const res = await fetch(`http://localhost:8000/follow/add/${userId}`, {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    credentials: "include",
+                });
+                const data = await res.json();
+                console.log(data);
+
+                if (!res.ok) {
+                    console.error(res);
+                    return;
+                }
+                setIsFollowed(true);
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+
+
+    return (
+        <div className="follow-button">
+            <button
+                onMouseEnter={() => setIsHovering(true)}
+                onMouseLeave={() => setIsHovering(false)}
+                className={isFollowed ? "followed" : "not-followed"}
+                onClick={handleClick}>{isFollowed ? isHovering ? "Se désabonner" : "Abonné" : "Suivre"}</button>
+        </div>
+    )
+}
--- front-end/src/components/profil/ProfilInfo.tsx
+++ front-end/src/components/profil/ProfilInfo.tsx
@@ -1,5 +1,6 @@
 import type { UserType } from "../../types/UserType"
 import GoBackComponenet from "../global/GoBackComponenet"
+import FollowComponent from "../global/FollowComponent";
 
 
 export default function ProfilInfo({ userPage }: { userPage: UserType }) {
@@ -12,12 +13,14 @@ export default function ProfilInfo({ userPage }: { userPage: UserType }) {
             <div className="go-back">
                 <GoBackComponenet />
                 <h1>{userPage.name} {userPage.lastname}</h1>
-
             </div>
             <div className="profil-info">
-                <div className="profil-info-name">
-                    <h1>{userPage.name} {userPage.lastname}</h1>
-                    <p>@{userPage.username}</p>
+                <div className="profil-info-top">
+                    <div className="profil-info-name">
+                        <h1>{userPage.name} {userPage.lastname}</h1>
+                        <p>@{userPage.username}</p>
+                    </div>
+                    <FollowComponent userId={userPage.id}/>
                 </div>
                 <div></div>
                 <div></div>
